b7f855c32da24b60a2f0f53ecb4cf4743c7abf08
Laurie Voss <github@seldo.com>
2019-11-01 13:02:40 -0700

    Last blog post

---
+++ index.js
@@ -0,0 +1,25 @@
+const csv = require('fast-csv');
+const fs = require('fs')
+
+let flatData = []
+
+csv
+    .parseFile('./presidents.csv',{headers:true})
+    .on('error', error => console.error(error))
+    .on('data', row => flatData.push(row))
+    .on('end', rowCount => {
+        let entries = flatData.map((row) => {
+            return `<li>
+                <h2>${row['Name']}</h2>
+                <h3><a href="${row['Book Link']}">${row['Book']}</a> by ${row['Book Author']}</h3>
+                <p>${ (row['Recommend?'] == 'Y') ? 'Recommended. ' : 'Not recommended. ' }${row['Book mini-review']}</p>
+                <blockquote class="twitter-tweet">
+                    <a href="${row['Tweets']}">Tweet</a>
+                </blockquote>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
+            </li>`
+        })
+
+        //fs.writeFileSync('./data/slides.json', JSON.stringify(structuredData,null,1))
+        console.log(entries.join("\n"))
+    });
