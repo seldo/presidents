5badfc27da6bd4b62b004064b9baa109c1b8cbe7
Jamie Kyle <me@thejameskyle.com>
2024-02-27 23:29:46 -0800

    Format all the files

--- presidents/src/templates/president-page.js
+++ presidents/src/templates/president-page.js
@@ -1,23 +1,23 @@
 import React from "react"
-import Layout from "../components/layout"
 import FunFacts from "../components/funfacts"
+import Layout from "../components/layout"
 
 import SEO from "../components/seo"
 
 export default ({ pageContext }) => {
-    let president = pageContext.president
-    return (
-        <Layout>
-            <SEO
-                title={`President ${president.President}`}
-                description="A list of all the US presidents, arbitrarily ranked by @seldo"
-            />
-            <div className="presidentPage">
-                <FunFacts president={president}/>
-                <div class="backCTA">
-                    <a href="/">Back to index</a>
-                </div>
-            </div>
-        </Layout>
-    )
+  let president = pageContext.president
+  return (
+    <Layout>
+      <SEO
+        title={`President ${president.President}`}
+        description="A list of all the US presidents, arbitrarily ranked by @seldo"
+      />
+      <div className="presidentPage">
+        <FunFacts president={president} />
+        <div class="backCTA">
+          <a href="/">Back to index</a>
+        </div>
+      </div>
+    </Layout>
+  )
 }
