7cfe38c78eb7b87bd413dd6d2fa8aad140efeed5
Jamie Kyle <me@thejameskyle.com>
2024-02-27 23:31:20 -0800

    Temp use local tiers.json file

--- presidents/gatsby-node.js
+++ presidents/gatsby-node.js
@@ -5,37 +5,37 @@
  */
 const path = require("path")
 
-if (!process.env.GOOGLE_API_KEY) {
-  throw new Error("Must define env var GOOGLE_API_KEY")
-}
-const call =
-  "https://sheets.googleapis.com/v4/spreadsheets/14pdn9yikv2ET9SNz2mcdAoh4On5sgHfkI-HbCEfrLdI/values/'Tiers'!A:P?key=" +
-  process.env.GOOGLE_API_KEY
-let presidents = []
+// if (!process.env.GOOGLE_API_KEY) {
+//   throw new Error("Must define env var GOOGLE_API_KEY")
+// }
+// const call =
+//   "https://sheets.googleapis.com/v4/spreadsheets/14pdn9yikv2ET9SNz2mcdAoh4On5sgHfkI-HbCEfrLdI/values/'Tiers'!A:P?key=" +
+//   process.env.GOOGLE_API_KEY
+let presidents = require("./tiers.json")
 
-const getPresidents = async () => {
-  // get the presidents
-  try {
-    let fetch = await import("node-fetch")
-    let response = await fetch(call)
-    let json = await response.json()
-    let headers = json.values.shift()
-    presidents = json.values.map(president => {
-      let row = {}
-      headers.forEach((header, index) => {
-        row[header] = president[index]
-      })
-      return row
-    })
-  } catch (e) {
-    console.log(`Failed to fetch presidents`)
-    console.log(e)
-  }
-}
+// const getPresidents = async () => {
+//   // get the presidents
+//   try {
+//     let fetch = await import("node-fetch")
+//     let response = await fetch(call)
+//     let json = await response.json()
+//     let headers = json.values.shift()
+//     presidents = json.values.map(president => {
+//       let row = {}
+//       headers.forEach((header, index) => {
+//         row[header] = president[index]
+//       })
+//       return row
+//     })
+//   } catch (e) {
+//     console.log(`Failed to fetch presidents`)
+//     console.log(e)
+//   }
+// }
 
-exports.onPreBootstrap = async () => {
-  await getPresidents()
-}
+// exports.onPreBootstrap = async () => {
+//   await getPresidents()
+// }
 
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage, deletePage } = actions
